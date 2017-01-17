d12e390e737e395476152eb16e9f893f3a256ec0
jechandler <jechandler00@gmail.com>
2017-01-16 17:00:00 -0800

    Add files via upload
    Input and Output for user console

---
+++ QuizMachine/src/quizmachine/QuizMachine.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+package quizmachine;
+import java.util.*;
+
+
+
+/**
+ *
+ * @author Jonathan Chandler 
+ */
+public class QuizMachine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Accounts application = new Accounts();    //instantiate a new Wages object 
+                                              //  called application  
+
+         application.accounttype();   
+        
+    
+       
+       
+    }
+    
+}
